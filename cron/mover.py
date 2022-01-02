401f41ab6fb63c5e100c2f540221c6e9d7a233f6
Steven Jung <37044660+jungs1@users.noreply.github.com>
2022-01-02 16:28:11 +0900

    Add cron python script for Plex torrent on complete

---
+++ cron/mover.py
@@ -0,0 +1,45 @@
+import logging
+import os
+import difflib
+import shutil
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler(os.path.join(dir_path, 'events.log'))
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(file_handler)
+
+def main():
+    for root, dirs, files in os.walk('/jung-nas/torrents/completed', topdown=True):
+        for dir in dirs:
+            dir_to_move = find_best_dir(dir, '/jung-nas/media')
+            shutil.move(os.path.join(root, dir), dir_to_move)
+            logger.info("FOLDER: Moved {} to {}".format(dir, dir_to_move))
+        for file in files:
+            name, ext = os.path.splitext(file)
+            if ext == '.iso':
+                dir_to_move = find_best_dir(name, '/jung-nas/linux_iso')
+                shutil.move(os.path.join(root, file), dir_to_move)
+                logger.info("ISO: Moved {} to {}".format(file, dir_to_move))
+            elif ext in ['.mkv', '.mp4']:
+                dir_to_move = find_best_dir(name, '/jung-nas/media')
+                shutil.move(os.path.join(root, file), dir_to_move)
+                logger.info("MEDIA: Moved {} to {}".format(file, dir_to_move))
+        break
+def find_best_dir(file, dir_to_look):
+    max_ratio = 0
+    dir_to_move = None
+    for root, dirs, files in os.walk(dir_to_look, topdown=False):
+        for dir in dirs:
+            ratio = difflib.SequenceMatcher(None, file, os.path.join(root, dir)).ratio()
+            if ratio > max_ratio:
+                max_ratio = ratio
+                dir_to_move = os.path.join(root, dir)
+
+    return dir_to_move
+
+if __name__ == '__main__':
+    main()
