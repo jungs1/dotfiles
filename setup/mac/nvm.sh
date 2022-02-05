0440b3a9ad4b8ab4d5e5c329d98fd1bd7595fa1f
Steven Jung <37044660+jungs1@users.noreply.github.com>
2022-02-05 19:20:03 +0900

    Add docker-compose files for server

--- setup/mac/nvm.sh
+++ setup/mac/nvm.sh
@@ -4,7 +4,7 @@ nvmrc=~/.nvm/nvm.sh
 if [ -e $nvmrc ]; then
     echo "Node $(node --version) & NPM $(npm --version) already installed"
     source $nvmrc
-    nvm use
+    # nvm use
 else
     echo "Installing nvm"
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
