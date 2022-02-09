0b5408b4b382fdf40b49b46818c261bdb646c00b
Steven Jung <37044660+jungs1@users.noreply.github.com>
2022-02-09 11:20:31 +0900

    Update p10k configuration and setup nvim

--- setup/mac/brew.sh
+++ setup/mac/brew.sh
@@ -7,11 +7,6 @@ else
     echo "Installing Homebrew"
     # nvm
     export NVM_DIR="$HOME/.nvm"
-
-    # Load nvm
-    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"     
     # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    # brew bundle --file=setup/mac/Brewfile --verbose
 fi
 
