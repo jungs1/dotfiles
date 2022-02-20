3bdafdc28a9f0a9321b75eaa6f7028d51da9e3e8
Steven Jung <37044660+jungs1@users.noreply.github.com>
2022-02-20 16:40:30 +0900

    Remove neovim config and update Brewfile

--- cron/mover.py
+++ cron/mover.py
@@ -12,27 +12,36 @@ file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 logger.addHandler(file_handler)
 
+TORRENT_COMPLETED_DIR= "/media/nuclear/torrents/completed"
+BASE_MEDIA_DIR = "/media/nuclear/media"
+# SET YOUR OWN DIR 
+ISO_DIR = ""
+MEDIA_DIR = ""
+
 def main():
-    for root, dirs, files in os.walk('/jung-nas/torrents/completed', topdown=True):
+    for root, dirs, files in os.walk(TORRENT_COMPLETED_DIR, topdown=True):
         for dir in dirs:
-            dir_to_move = find_best_dir(dir, '/jung-nas/media')
+            dir_to_move = find_best_dir(dir, BASE_MEDIA_DIR)
             shutil.move(os.path.join(root, dir), dir_to_move)
-            logger.info("FOLDER: Moved {} to {}".format(dir, dir_to_move))
+            logger.info("FOLDER => Moved from {} to {}".format(dir, dir_to_move))
         for file in files:
             name, ext = os.path.splitext(file)
+            # iso files
             if ext == '.iso':
-                dir_to_move = find_best_dir(name, '/jung-nas/linux_iso')
+                dir_to_move = find_best_dir(name, ISO_DIR)
                 shutil.move(os.path.join(root, file), dir_to_move)
-                logger.info("ISO: Moved {} to {}".format(file, dir_to_move))
+                logger.info("ISO => Moved from {} to {}".format(file, dir_to_move))
+            # media files
             elif ext in ['.mkv', '.mp4']:
-                dir_to_move = find_best_dir(name, '/jung-nas/media')
+                dir_to_move = find_best_dir(MEDIA_DIR)
                 shutil.move(os.path.join(root, file), dir_to_move)
-                logger.info("MEDIA: Moved {} to {}".format(file, dir_to_move))
+                logger.info("MEDIA => Moved from {} to {}".format(file, dir_to_move))
         break
+
 def find_best_dir(file, dir_to_look):
     max_ratio = 0
     dir_to_move = None
-    for root, dirs, files in os.walk(dir_to_look, topdown=False):
+    for root, dirs, _ in os.walk(dir_to_look, topdown=False):
         for dir in dirs:
             ratio = difflib.SequenceMatcher(None, file, os.path.join(root, dir)).ratio()
             if ratio > max_ratio:
