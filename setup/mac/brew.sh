1adc5a0fdb7414bccf36a52fe4a221d72680b969
Steven Jung <37044660+jungs1@users.noreply.github.com>
2022-02-23 17:58:08 +0900

    Update homebrew setup

--- setup/mac/brew.sh
+++ setup/mac/brew.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env zsh
 echo '\n<<< Starting Homebrew Setup >>>\n'
 
-if exists nvm; then
-    echo 'Homebrew already installed'
-else 
+if ! which brew; then
     echo "Installing Homebrew"
-    # nvm
-    export NVM_DIR="$HOME/.nvm"
-    # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else 
+    echo 'Homebrew already installed'
 fi
 
