675e9313bd13de022a5643e08503f31eb405de9a
Steven Jung <37044660+jungs1@users.noreply.github.com>
2022-03-17 08:38:24 +0900

    Add custom theme and update automatic installs

--- setup/mac/nvm.sh
+++ setup/mac/nvm.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env zsh
 echo '\n<<< Starting nvm Setup >>>\n'
 
-nvmrc=~/.nvm/nvm.sh
-if [ -e $nvmrc ]; then
+if [ -f "$HOME/.nvm/nvm.sh" ]; then
     echo "Node $(node --version) & NPM $(npm --version) already installed"
-    source $nvmrc
 else
     echo "Installing nvm"
+    rm -rf ~/.nvm
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
     source ~/.zshrc
     nvm install --lts
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env zsh
 echo '\n<<< Starting plugins Setup >>>\n'
 
-if [ -d "/Users/swj/.zsh/zsh-autosuggestions" ] 
+if [ -d "$HOME/.zsh/zsh-autosuggestions" ] 
 then
     echo "Already installed zsh-autosuggestion!" 
 else
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.zsh/zsh-autosuggestions
 fi
 
-if [ -d "/Users/swj/.zsh/zsh-syntax-highlighting" ] 
+if [ -d "$HOME/.zsh/zsh-syntax-highlighting" ] 
 then
     echo "Already installed zsh-syntax-highlighting!" 
 else
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/zsh-syntax-highlighting
 fi
